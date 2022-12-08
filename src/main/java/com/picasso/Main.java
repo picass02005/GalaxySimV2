edfcda33e7d1a29685909a1e02c196a8df8c2e52
picasso2005 <clementduran0@gmail.com>
2022-12-08 22:53:56 +0100

    Update todos

--- src/main/java/com/picasso/Main.java
+++ src/main/java/com/picasso/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         List<Planet> planets = new ArrayList<>();
 
-        planets.add(new Planet.Builder().mass(2).position(5D, 1D).velocity(new Vector(0, 0)).build());
-        planets.add(new Planet.Builder().mass(1).position(0D, 0D).velocity(new Vector(0, 0)).build());
+        planets.add(new Planet.Builder().mass(5000).position(5D, 1D).velocity(new Vector(0, 0)).build());
+        planets.add(new Planet.Builder().mass(200).position(0D, 0D).velocity(new Vector(0, 0)).build());
 
         for (int i = 0; i < 5; i++) {
             for (Planet planet : planets) {
@@ -18,3 +18,7 @@ public class Main {
         }
     }
 }
+
+// TODO: calculate it in parallel to increase calculation speed
+// TODO: find a way to calculate faster for high speed simulation
+// TODO: config file
