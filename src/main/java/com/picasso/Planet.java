31d6595dc0e23bc055e47bbee4aef04b23951c62
picasso2005 <clementduran0@gmail.com>
2022-12-08 21:37:31 +0100

    Planets now have a method to calculate new velocity (after 1 unit time)

--- src/main/java/com/picasso/Planet.java
+++ src/main/java/com/picasso/Planet.java
@@ -1,9 +1,10 @@
 package com.picasso;
 
 import java.awt.*;
+import java.util.List;
 import java.util.Random;
 
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
 
 /**
  * Class used to represent a planet and calculate positions after each steps
@@ -14,8 +15,8 @@ public class Planet {
     public final int mass;
     public final Color color;
     private final double G = (6.6743 * pow(10, -11));
-    public int x;
-    public int y;
+    public Float x;
+    public Float y;
     public Vector velocity;
 
     /**
@@ -31,16 +32,42 @@ public class Planet {
         this.color = builder.color;
     }
 
+    private double calcHalfCoordinate(float a, float b, double ab, double acceleration) {
+        return 1 / ab * abs(acceleration) * (b - a) + a;
+    }
+
+    private void calculateNewVelocity(List<Planet> planets) {
+        Vector accelVector = new Vector(0, 0);
+
+        for (Planet planet : planets) {
+            if (planet != this) {
+                // d = sqrt(x^2 + y^2)
+                double distance = sqrt(pow(this.x - planet.x, 2) + pow(this.y - planet.y, 2));
+
+                // F = G * ((m1 * m2) / d^2)
+                // a = F / m
+                double gravAccel = this.mass / (this.G * ((this.mass * planet.mass) / pow(distance, 2)));
+
+                accelVector.addVector(new Vector(
+                        calcHalfCoordinate(this.x, planet.x, distance, gravAccel),
+                        calcHalfCoordinate(this.y, planet.y, distance, gravAccel)
+                ));
+            }
+        }
+
+        this.velocity.addVector(accelVector);
+    }
+
     /**
      * Builder class made to generate all parameters for Planet class if user don't give them
      *
      * @author picasso2005
      */
     public static class Builder {
-        private int x = 0;
-        private int y = 0;
+        private Float x;
+        private Float y;
         private Vector velocity = null;
-        private int mass = 0;
+        private Integer mass = 0;
         private Color color = null;
 
         /**
@@ -50,7 +77,7 @@ public class Planet {
          * @param y The desired y coordinate
          * @return Builder with this parameter updated
          */
-        public Builder position(int x, int y) {
+        public Builder position(Float x, Float y) {
             this.x = x;
             this.y = y;
 
@@ -76,7 +103,7 @@ public class Planet {
          * @param mass The planet desired mass
          * @return Builder with this parameter updated
          */
-        public Builder mass(int mass) {
+        public Builder mass(Integer mass) {
             this.mass = mass;
 
             return this;
@@ -103,14 +130,14 @@ public class Planet {
         public Planet build() {
             final Random rand = new Random();
 
-            if (this.x == 0) {
+            if (this.x == null) {
                 final int max_x = 1920; // TODO: config file
-                this.x = rand.nextInt(max_x * 2) - max_x;
+                this.x = rand.nextFloat(max_x * 2) - max_x;
             }
 
-            if (this.y == 0) {
+            if (this.y == null) {
                 final int max_y = 1080; // TODO: config file
-                this.y = rand.nextInt(max_y * 2) - max_y;
+                this.y = rand.nextFloat(max_y * 2) - max_y;
             }
 
             if (this.velocity == null) {
@@ -118,7 +145,7 @@ public class Planet {
                 this.velocity = new Vector(rand.nextFloat(-max, max), rand.nextFloat(-max, max));
             }
 
-            if (this.mass == 0) {
+            if (this.mass == null) {
                 mass = rand.nextInt(1, 20);  // TODO: config file
             }
 
